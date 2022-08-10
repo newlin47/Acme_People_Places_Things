584b18719b149db5c363e634be33600c04ebee94
Ethan Newlin <newlin5@gmail.com>
2022-08-09 21:33:06 -0400

    finished module

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const { conn, Person, Place, Thing, Souvenir, syncAndSeed } = require('./db');
-
 const app = express();
+app.use(require('method-override')('_method'));
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', async (req, res, next) => {
 	try {
@@ -53,14 +54,76 @@ app.get('/', async (req, res, next) => {
                     <ul>
                         ${souvenirs
 													.map((souvenir) => {
-														return `<li>${souvenir.person.name} purchased a ${souvenir.thing.name} in ${souvenir.place.name}</li>`;
+														return `<li>${souvenir.person.name} purchased a ${souvenir.thing.name} in ${souvenir.place.name} <form method='POST' action='/${souvenir.id}?_method=DELETE'>
+                                                        <button>
+                                                        Delete 
+                                                        </button>
+                                                      </form></li>`;
 													})
 													.join('')}
                     </ul>
             </div>
+            <div>
+                <form method='POST' action='/'>
+                    <select name='personId'>
+                    ${persons
+											.map((person) => {
+												return `
+                            <option value=${person.id}>
+                            ${person.name}
+                            </option>
+                        `;
+											})
+											.join('')}
+                    </select>
+                    <select name='placeId'>
+                    ${places
+											.map((place) => {
+												return `
+                            <option value=${place.id}>
+                            ${place.name}
+                            </option>
+                        `;
+											})
+											.join('')}
+                    </select>
+                    <select name='thingId'>
+                    ${things
+											.map((thing) => {
+												return `
+                            <option value=${thing.id}>
+                            ${thing.name}
+                            </option>
+                        `;
+											})
+											.join('')}
+                    </select>
+                    
+                    <button>Create</button>
+                </form>
+            </div>
             </body>
         </html>
         `);
+	} catch (error) {
+		next(error);
+	}
+});
+
+app.post('/', async (req, res, next) => {
+	try {
+		await Souvenir.create(req.body);
+		res.redirect('/');
+	} catch (error) {
+		next(error);
+	}
+});
+
+app.delete('/:id', async (req, res, next) => {
+	try {
+		const souvenirDead = await Souvenir.findByPk(req.params.id);
+		await souvenirDead.destroy();
+		res.redirect('/');
 	} catch (error) {
 		next(error);
 	}
